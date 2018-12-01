1848d95f5785953643330ecdd24723a6b399329c
Melgui <gerard.melgares@students.salle.url.edu>
2018-12-01 16:20:34 +0100

    feat: Impplemented Json
    Hem implementat la lectura dels fitxers json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
 import com.google.gson.Gson;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        /**
+         * Llegim el fitxer Json
+         */
         Gson gson = new Gson();
-        FileReader f = new FileReader("src/main/java")
+        FileReader f = new FileReader("datasets/xs_dataset.json");
+        User[] users = gson.fromJson(f, User[].class);
+
     }
 }

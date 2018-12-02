c1ee42307fee4e2373df22d5fee19969cd73db58
joseproig <josep.roig@students.salle.url.edu>
2018-12-02 12:59:29 +0100

    docs: Added de JavaDoc to the Post Class
    He afegit els comentaris de JavaDoc a la classe Post

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -22,70 +22,133 @@ public class Post {
         this.usersComments = new ArrayList<User>();
     }
 
+    /**
+     * Getter de ID
+     * @return Valor actual de ID
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Setter de ID
+     * @param id Valor que establim a ID
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Getter de Published
+     * @return Valor actual de published
+     */
     public int getPublished() {
         return published;
     }
 
+    /**
+     * Setter de Published
+     * @param published Valor que establim a Published
+     */
     public void setPublished(int published) {
         this.published = published;
     }
 
+    /**
+     * Getter de Location
+     * @return Valors actuals que té la l'atribut de tipus llista Location
+     */
     public List<Double> getLocation() {
         return location;
     }
 
+    /**
+     * Setter de Location
+     * @param location Valors que volem assignar a l'atribut tipus llista location
+     */
     public void setLocation(List<Double> location) {
         this.location = location;
     }
 
+    /**
+     * Getter  de Category
+     * @return Valor actual de Category
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * Setter de Category
+     * @param category Valor que volem establir a l'atribut Category
+     */
     public void setCategory(String category) {
         this.category = category;
     }
 
+    /**
+     * Getter de liked_by
+     * @return Valor actual de l'atribut llista liked_by
+     */
     public List<String> getLiked_by() {
         return liked_by;
     }
 
+    /**
+     * Setter de liked_by
+     * @param liked_by Valors que volem assignar a l'atribut tipus llista liked_by
+     */
     public void setLiked_by(List<String> liked_by) {
         this.liked_by = liked_by;
     }
 
+    /**
+     * Getter de commented_by
+     * @return Valor actual de l'atribut tipus llista commented_by
+     */
     public List<String> getCommented_by() {
         return commented_by;
     }
 
+    /**
+     * Setter de commented_by
+     * @param commented_by Valors que volem assignar a l'atribut de tipus llista commented_by
+     */
     public void setCommented_by(List<String> commented_by) {
         this.commented_by = commented_by;
     }
 
+    /**
+     * Getter de usersLikes
+     * @return Valor actual de l'atribut tipus llista usersLikes
+     */
     public List<User> getUsersLikes() {
         return usersLikes;
     }
 
+    /**
+     * Setter de usersLikes
+     * @param usersLikes Valor que assignem a l'atribut de tipus llista usersLikes
+     */
     public void setUsersLikes(List<User> usersLikes) {
         this.usersLikes = usersLikes;
     }
 
+    /**
+     * Getter de usersComments
+     * @return Valor actual de l'atribut tipus llista usersComments
+     */
     public List<User> getUsersComments() {
         return usersComments;
     }
 
+    /**
+     * Setter de usersComments
+     * @param usersComments Valor que volem assignar a l'atribut tipus llista usersComments
+     */
     public void setUsersComments(List<User> usersComments) {
         this.usersComments = usersComments;
     }
-
     /**
      * Funcio que ens permet referenciar els usuaris que han donat Like a un post concret
      * @param u Indica tots els usuaris de la plataforma, cosa que ens serveix per poder buscar-los per fer la

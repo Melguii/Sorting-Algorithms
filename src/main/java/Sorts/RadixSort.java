78385358dcb1f24eefce21911b316194e2e11938
Melgui <gerard.melgares@students.salle.url.edu>
2018-12-04 23:55:18 +0100

    feat: nueva versión radix sort, sigue en mal estado
    Mini actualitzacio del counting sort interior

--- src/main/java/Sorts/RadixSort.java
+++ src/main/java/Sorts/RadixSort.java
@@ -3,6 +3,7 @@ package Sorts;
 import Compare.Comparator;
 import JSONClasses.Post;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RadixSort {
@@ -43,15 +44,31 @@ public class RadixSort {
         return numAux;
     }
 
-    private List<Post> countingSort(List<Post> p, int div){
-        int
-        int unitat[] =
+    private List<Post> countingSort(List<Post> p, int div) {
+        List<Post> aux = new ArrayList<Post>();
+        List<Post> comptadorDig = new ArrayList<Post>();
 
-        for(int i = 0; i < p.size(); i++){
+        //Mirem quants numeros de cadam tenim entre 0 al 9
+        for (int i = 0; i < p.size(); i++) {
+            comptadorDig[(p.get(i)/div)%10]++;
+        }
 
+        //Mtijancant un canvi necessari (sumes entre posicions anteriors i actuals) fem que el nostre array comptador apunti a les caselles
+        //que li pertoquen de l'array aux
+        for (int i = 1; i < 19; i++) {
+            comptadorDig[i] += comptadorDig[i - 1];
+        }
+
+        //Subtituim en els avalors de aux els nous valors ja ordenats per digits previament
+        for (int i = p.size() - 1; i >= 0; i++) {
+            aux
+            comptadorDig
+        }
+
+        //Tornem a passar un array extern a un altre la informacio necessaria
+        for (int i = 0; i < p.size(); i++) {
+            p.set(i, aux.get(i));
         }
     }
-
-    private
 
 }
